b6218ffa6848e5bc6afd701985977da2dec7d2b8
Dhanush-Zemoso <86592944+Dhanush-Zemoso@users.noreply.github.com>
2021-09-15 09:46:55 +0530

    HTML-CSS-Final

---
+++ HTML&CSS/JavaScriptAssignment2/data.js
@@ -0,0 +1,77 @@
+let menuList = [{
+        'id': 1,
+        'name': 'french fries',
+        'type': 'snacks',
+        'cost': 150
+    },
+    {
+        'id': 2,
+        'name': 'aloo tikki',
+        'type': 'snacks',
+        'cost': 220
+    },
+    {
+        'id': 3,
+        'name': 'hyderabadi dum biryani',
+        'type': 'main',
+        'cost': 300
+    },
+    {
+        'id': 4,
+        'name': 'veg nuggets',
+        'type': 'snacks',
+        'cost': 250
+    },
+    {
+        'id': 5,
+        'name': 'paneer tikki biryani',
+        'type': 'main',
+        'cost': 320
+    },
+    {
+        'id': 6,
+        'name': 'handi biryani',
+        'type': 'main',
+        'cost': 350
+    },
+    {
+        'id': 7,
+        'name': 'tomato soup',
+        'type': 'starter',
+        'cost': 180
+    },
+    {
+        'id': 8,
+        'name': 'chocolate icecream',
+        'type': 'desert',
+        'cost': 150
+    },
+    {
+        'id': 9,
+        'name': 'gulab jamun',
+        'type': 'desert',
+        'cost': 120
+    },
+    {
+        'id': 10,
+        'name': 'mojito',
+        'type': 'beverage',
+        'cost': 150
+    },
+    {
+        'id': 11,
+        'name': 'chocolate thickshake',
+        'type': 'beverage',
+        'cost': 170
+    }
+]
+
+/*
+,
+    {
+        'id':,
+        'name':'',
+        'type':'',
+        'cost':
+    }
+*/
